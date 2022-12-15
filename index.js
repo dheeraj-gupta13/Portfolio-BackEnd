e18ca16d0d3b1a0f965aede6e03eb2ec99470640
Dheeraj Gupta <dj13042002@gmail.com>
2022-12-16 01:30:36 +0530

    resolving cors

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+//other imports
+app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true
+    })
+)
 
 // app.use(
 //     cors({origin:"http://localhost:3000"})
